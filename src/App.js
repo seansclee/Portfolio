baef29665b04f32f98173e2bd0facf51e51e4553
Sean Lee <seanslee92@gmail.com>
2018-12-21 21:30:25 -0500

    nav menu created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component, Fragment } from 'react'
 import Splash from './logo1.png'
+import Logo from './logo.png'
 import './App.css'
-import { Dropdown, Menu, Button, Form, Grid, Header, Image, Message, Segment, Transition } from 'semantic-ui-react'
+import Navbar from './Navbar.js'
+import { Dropdown, Menu, Button, Form, Grid, Header, Image, Message, Segment, Transition, Input, Container } from 'semantic-ui-react'
 
 class App extends Component {
-  state = { splash: true, visible: false, activeItem: 'account' }
+  state = { splash: true, visible: false, activeItem: 'home' }
 
   componentDidMount() {
-    this.setState({ splash: true, visible: true, activeItem: 'account' })
-    setTimeout(() => this.setState({ splash: true, visible: false, activeItem: 'account' }), 3000)
-    setTimeout(() => this.setState({ splash: false, visible: true, activeItem: 'account' }), 3500)
+    this.setState({ splash: true, visible: true, activeItem: 'home' })
+    setTimeout(() => this.setState({ splash: true, visible: false, activeItem: 'home' }), 3000)
+    setTimeout(() => this.setState({ splash: false, visible: false, activeItem: 'home' }), 3500)
   }
 
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
@@ -20,47 +22,50 @@ class App extends Component {
     return (
       splash
         ?
-        <div className='login-form'>
+        <div className='splash'>
           <style>{`
         body > div,
         body > div > div,
-        body > div > div > div.login-form {
+        body > div > div > div.splash {
           height: 100%;
         }
       `}</style>
           <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
             <Grid.Column>
               <Transition visible={visible} animation='scale' duration={500}>
-                <Image src={Splash} size='medium' centered />
+                <Image src={Splash} size='large' centered />
               </Transition>
             </Grid.Column>
           </Grid>
         </div>
         :
-        <Transition visible={visible} animation='scale' duration={500}>
-          <Fragment>
-            <Menu secondary vertical fixed='right'>
-              <Menu.Item
-                name='account'
-                active={activeItem === 'account'}
-                onClick={this.handleItemClick}
-              />
-              <Menu.Item
-                name='settings'
-                active={activeItem === 'settings'}
-                onClick={this.handleItemClick}
-              />
-              <Dropdown item text='Display Options'>
-                <Dropdown.Menu>
-                  <Dropdown.Header>Text Size</Dropdown.Header>
-                  <Dropdown.Item>Small</Dropdown.Item>
-                  <Dropdown.Item>Medium</Dropdown.Item>
-                  <Dropdown.Item>Large</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </Menu>
-          </Fragment>
-        </Transition>
+        <Container style={{ marginTop: '1em' }}>
+          <Menu secondary>
+            <Menu.Item>
+              <Image width='70em' height='70em' src={Logo} href='/' />
+            </Menu.Item>
+            <Menu.Item
+              name='home'
+              active={activeItem === 'home'}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              name='projects'
+              active={activeItem === 'projects'}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              name='about me'
+              active={activeItem === 'about me'}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              name='contact'
+              active={activeItem === 'contact'}
+              onClick={this.handleItemClick}
+            />
+          </Menu>
+        </Container>
     );
   }
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './Navbar.css';
+import React, { Component } from 'react'
+import logo from './logo1.png'
+import { Dropdown, Menu, Container, Button, Form, Grid, Header, Image, Message, Segment, Transition } from 'semantic-ui-react'
 
-class Navbar extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const Navbar = () => (
+  <Menu fixed='top' inverted>
+    <Container>
+      <Menu.Item as='a' header>
+        <Image size='mini' src={logo} style={{ marginRight: '1.5em' }} />
+        Hi
+        </Menu.Item>
+      <Menu.Item as='a'>Home</Menu.Item>
+    </Container>
+  </Menu>
+)
 
 export default Navbar;
