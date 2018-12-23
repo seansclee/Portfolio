990946a9ba1cec636e6ff6d05753c27b6d8ea341
Sean Lee <seanslee92@gmail.com>
2018-12-23 06:22:28 -0500

    functionality finished, just need to add content

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import Logo from './logo.png'
 import './App.css'
 import Home from './Home.js'
 import About from './About.js'
+import Projects from './Projects.js'
+import Contact from './Contact.js'
 import { Dropdown, Menu, Button, Form, Grid, Header, Image, Message, Segment, Transition, Input, Container } from 'semantic-ui-react'
 
-const pages = { 'home': <Home/>, 'about me': <About/>}
+const pages = { 'home': <Home/>, 'about me': <About/>, 'projects': <Projects/>, 'contact': <Contact/> }
 
 class App extends Component {
   state = { splash: true, visible: false }
 
   componentDidMount() {
     this.setState({ splash: true, visible: true })
-    setTimeout(() => this.setState({ splash: true, visible: false }), 3000)
-    setTimeout(() => this.setState({ splash: false, visible: false, activeItem: 'home', currentView: <Home/> }), 3500)
+    setTimeout(() => this.setState({ splash: true, visible: false }), 2500)
+    setTimeout(() => this.setState({ splash: false, visible: false, activeItem: 'home', currentView: <Home /> }), 3000)
   }
 
   handleItemClick = (e, { name }) => this.setState({ activeItem: name, currentView: pages[name] })
@@ -25,13 +27,7 @@ class App extends Component {
       splash
         ?
         <div className='splash'>
-          <style>{`
-        body > div,
-        body > div > div,
-        body > div > div > div.splash {
-          height: 100%;
-        }
-      `}</style>
+          <style>{`body > div, body > div > div, body > div > div > div.splash { height: 100%; }`}</style>
           <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
             <Grid.Column>
               <Transition visible={visible} animation='scale' duration={500}>
@@ -42,8 +38,9 @@ class App extends Component {
         </div>
         :
         <div className='main'>
+          <style>{`body > div, body > div > div, body > div > div > div.main { height: 100%; }`}</style>
           <Container>
-            <Menu size='huge' secondary style={{paddingTop: '1em'}}>
+            <Menu size='huge' secondary style={{ paddingTop: '1em' }}>
               <Menu.Item>
                 <Image width='80em' height='80em' src={Logo} href='/' />
               </Menu.Item>
@@ -70,9 +67,7 @@ class App extends Component {
                 />
               </Menu.Menu>
             </Menu>
-            <Transition animation='fade' duration='500'>
-            { currentView }
-            </Transition>
+            {currentView}
           </Container>
         </div>
     );
